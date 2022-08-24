611d33fb1f260920d6532450437c6cda83b15b3d
Joel Plaza <joel.plaza02@gmail.com>
2022-08-23 21:03:43 -0700

    fixed fontawesome import in navbar component

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoins } from '@fortawesome/free-solid-svg-icons';
 import './Navbar.css';
 
